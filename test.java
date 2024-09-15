e18d0c9c9eb03b2d80ba46c710d4b4ce6a3f2022
Robert Nguyen <150546895+Nguyen-Hung-Anh@users.noreply.github.com>
2024-09-15 18:49:33 -0400

    Update test.java

--- test.java
+++ test.java
@@ -4,6 +4,117 @@ import org.junit.jupiter.api.Test;
 import java.util.GregorianCalendar;
 import calendar.MeetingCalendar;
 
+Assessment report [-]
+OneTimeEvent should not displace existing meetings
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+PriorityEvent should displace existing meetings
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+WeeklyEvent should not displace existing meetings
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+MultiDayPerWeekEvent should not displace existing meetings
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+Must check that constructors initialize variables correctly
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+Must check that constructors initialize variables correctly
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+Must check all meetings generated in repeated events
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+Must make sure that repeated events stop when scheduled
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+Must be sure that multi-day events are not scheduling every day
+
+OUTPUT
+
+
+Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+
+WARNING: Delegated to the 'execute' command.
+         This behaviour has been deprecated and will be removed in a future release.
+         Please use the 'execute' command directly.
+
+
+
 class CalendarEventTest
 {
     private GregorianCalendar startTime;
